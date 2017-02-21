2d94d6eb8eb20841c7c3073cd320b862fe9a9ff1
Gayathri Mahalingam <mahalingamg@uncw.edu>
2017-02-21 12:22:53 -0800

    added more code to select tiles whose center is inside the selected polygon

--- imageCanvas.py
+++ imageCanvas.py
@@ -1,19 +1,33 @@
 
 import cv2
 import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+#from matplotlib.Artist import artist
+from matplotlib.mlab import dist_point_to_segment
+#from matplotlib.line import Lines2D
 
-class Canvas():
+class imageCanvas():
     def __init__(self,image):
         self.fname = image
         self.img = cv2.imread(self.fname)
         self.img_width = self.img.shape[1]
-        self.img_height = self.img_shape[0]
+        self.img_height = self.img.shape[0]
+        print self.img.shape
         self.point = ()
         self.polygon = []
+
+    def initFigure(self):
+        if self.img:
+            self.fig = plt.figure(figsize=(1,1))
+            self.ax = fig.add_subplot(111)
+            plt.imshow(self.img)
+            self.cid = self.fig.canvas.mpl_connect()
+
 
     def getCoord(self):
         fig = plt.figure()
         ax = fig.add_subplot(111)
+        ax.set_title(self.fname)
         plt.imshow(self.img)
         cid = fig.canvas.mpl_connect('button_press_event', self.__onclick__)
         plt.show()
@@ -21,6 +35,7 @@ class Canvas():
 
     def __onclick__(self,click):
         self.point = (click.xdata,click.ydata)
+        print self.point
         return self.point
 
     def convertPolygonPointsToWorld(self,bounds):
@@ -34,6 +49,8 @@ class Canvas():
         coords = [[]]
         if self.polygon:
             for x,y in self.polygon:
+                # convert the points to world coordinates
                 x = (x/scale) + bounds['minX']
                 y = (y/scale) + bounds['minY']
                 coords[0].append([x,y])
+        return coords
--- loadStack.py
+++ loadStack.py
@@ -4,7 +4,10 @@
 import os, sys, json
 import urllib
 import matplotlib.pyplot as plt
+import matplotlib.path as mplpath
+import numpy as np
 import cv2, PIL
+from imageCanvas import *
 
 
 def loadTileSpecs(stack,z):
@@ -31,6 +34,11 @@ def loadTileData(stackJsonData,stack):
         td["maxX"] = jt["maxX"]
         td["maxY"] = jt["maxY"]
         sectionId = jt["layout"]["sectionId"]
+
+        #compute tile centers in world coordinates
+        td["centerX"] = (td["maxX"]-td["minX"])/2
+        td["centerY"] = (td["maxY"]-td["minY"])/2
+
         # Not used at the moment
         #td["camera"] = jt["layout"]["camera"]
         #td["stageX"] = jt["layout"]["stageX"]
@@ -43,12 +51,33 @@ def loadTileData(stackJsonData,stack):
         tileData[sectionId] = td
     return tileData
 
-def convertPolygonPoints(points,bounds):
-    # points is an array of coordinates representing the polygon with first and last point being the same
-    bounds_width = bounds['maxX']-bounds['minX']
-    bounds_height = bounds['maxY']-bounds['minY']
+def selectTilesInsidePolygon(polygonPoints,tileData):
+    # polygonPoints is a set of coordinates that define the polygon boundary
+    # note that the first and last coordinates are not the same in polygonPoints
+    # returns a set of tile indices referencing tileData indices for those tiles whose centers are within the polygon
 
+    # extract the tile center from tileData
+    tileCenters = []
+    tileIds = []
+    for td in tileData:
+        tileCenters.append([td["centerX"], td["centerY"]])
+        tileIds.append(td["tileId"])
 
+    # create a bounding box path for the polygon
+    pPoints = np.array(polygonPoints)
+    bbpath = mplpath.Path(pPoints)
+
+    # check if the tile centers are within the polygon
+    contains = bbpath.contains_points(tileCenters)
+
+    # extract the indices of those tiles whose centers are within the polygon
+    indices = np.nonzero(contains==True)[0]
+
+    # extract the tile IDs from the indices
+    tileId = []
+    for i in indices:
+        tileId.append(tileIds[i])
+    return tileId
 
 
 if __name__ == '__main__':
@@ -62,11 +91,15 @@ if __name__ == '__main__':
     z = 2267
 
     tileSpecs = loadTileSpecs(stack,z)
-    #print tileSpecs[0]
+    print tileSpecs[0]
+
+    # get section ID
+    sectionId = tileSpecs[0]["layout"]["sectionId"]
+    print sectionId
 
     # get the bounds of all tiles in this section
     tileData = loadTileData(tileSpecs,stack)
-    print tileData[0]
+    print tileData[sectionId]
 
     # Read the downscaled image of each section
     files = os.listdir(downsampledImgPath)
@@ -75,6 +108,7 @@ if __name__ == '__main__':
         f = os.path.join(downsampledImgPath, f)
         if (os.path.isfile(f) and f.endswith(ext)):
             section = f[len(downsampledImgPath)+1:-3] + '0'
+            section = "2267.0"
 
             # read the down sampled image
             #img = cv2.imread(f)
@@ -89,7 +123,7 @@ if __name__ == '__main__':
             #scale = img_width*1.0/bounds_width
 
             # set up the image canvas to show the downscaled image and get the polgon roi
-            canvasImage = Canvas(f)
+            canvasImage = imageCanvas(f)
 
             bounds = {}
             bounds["minX"] = tileData[section]['minX']
@@ -97,9 +131,5 @@ if __name__ == '__main__':
             bounds["maxX"] = tileData[section]['maxX']
             bounds["maxY"] = tileData[section]['maxY']
 
-            # show the image and collect the polygon points
+            # show the image and collect the polygon points. The polygon points are saved in the canvas object
             canvasImage.getCoord()
-
-    # get section ID
-    sectionId = tileSpecs[0]["layout"]["sectionId"]
-    print sectionId
